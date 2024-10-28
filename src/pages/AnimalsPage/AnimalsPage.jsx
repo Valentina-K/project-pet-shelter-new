31c6a794e23f55076ad9c55116d34526356a0f71
Valentina-K <kvalentyna@hotmail.com>
2024-10-28 21:55:59 +0200

    feat: create search component, update AnimalPage (#10)

--- src/pages/AnimalsPage/AnimalsPage.jsx
+++ src/pages/AnimalsPage/AnimalsPage.jsx
@@ -1,14 +1,34 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import CardList from '../../components/CardList/CardList';
 import SideBar from '../../components/SideBar/SideBar';
-import { selectAdvertisements } from '../../redux/advertisements/selectors';
+import {
+  selectAdvertisements,
+  selectIsLoading,
+} from '../../redux/advertisements/selectors';
+import Search from '../../components/Search/Search';
+import { useEffect } from 'react';
+import { fetchAdvertisements } from '../../redux/advertisements/operations';
+import styles from './styles.module.css';
 
 function AnimalsPage() {
+  const dispatch = useDispatch();
   const ads = useSelector(selectAdvertisements);
+  const isLoading = useSelector(selectIsLoading);
+  //const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchAdvertisements());
+  }, [dispatch]);
+
   return (
-    <div>
-      <SideBar />
-      <CardList ads={ads} />
+    <div className={styles.pageContainer}>
+      <div className={styles.leftBlock}>
+        <SideBar />
+      </div>
+      <div className={styles.rightBlock}>
+        <Search />
+        {!isLoading && <CardList ads={ads} />}
+      </div>
     </div>
   );
 }
