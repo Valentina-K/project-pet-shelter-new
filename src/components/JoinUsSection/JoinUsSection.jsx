bd24fa9345872178310682144ce463ea31083b4b
Valentina-K <kvalentyna@hotmail.com>
2025-01-09 16:29:06 +0200

    fix title

--- src/components/JoinUsSection/JoinUsSection.jsx
+++ src/components/JoinUsSection/JoinUsSection.jsx
@@ -4,7 +4,9 @@ function JoinUsSection() {
   return (
     <div className={styles.wrapper}>
       <h2 className={styles.title}>
-        Running a shelter? Join us to make a bigger impact together!
+        Running a shelter?
+        <br />
+        Join us to make a bigger impact together!
       </h2>
       <button className={styles.button}>Join us</button>
     </div>
