1b0f448f8d45b2b8f0f10aaeeedb951b1dbddc94
Valentina-K <kvalentyna@hotmail.com>
2025-01-17 13:18:54 +0200

    create statistics section, fix subscript form, top-up button

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -20,6 +20,7 @@ import Shelters from '../../components/HomeComponents/Shelters/Shelters';
 import JoinUsSection from '../../components/HomeComponents/JoinUsSection/JoinUsSection';
 import OurBlog from '../../components/HomeComponents/OurBlog/OurBlog';
 import Subscribe from '../../components/HomeComponents/Subscription/Subscription';
+import Statistics from '../../components/HomeComponents/Statistics/Statistics';
 
 function HomePage() {
   const dispatch = useDispatch();
@@ -67,12 +68,8 @@ function HomePage() {
       <JoinUsSection />
       <OurBlog />
       <Subscribe />
-      {/*<HotAds />
-      
-      
-      <OurBlog />
-      
-      <Metrics /> */}
+      <Statistics />
+      {/*<HotAds />*/}
     </>
   );
 }
