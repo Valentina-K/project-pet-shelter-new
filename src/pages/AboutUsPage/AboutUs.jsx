bdf8467e47308e27b352d381367255bc68a553bf
KLEugene <kaeugene95@gmail.com>
2025-03-13 19:12:51 +0000

    added styles to AboutUsPAge without fixing

--- src/pages/AboutUsPage/AboutUs.jsx
+++ src/pages/AboutUsPage/AboutUs.jsx
@@ -1,20 +1,23 @@
 import styles from './styles.module.css';
-import temporaryImage from '../../assets/img/404-error-web-template-with-cute-dog_23-2147763341.jpg';
+import temporaryImage from '../../assets/img/subscript.png';
+import JoinUsSection from '../../components/HomeComponents/JoinUsSection/JoinUsSection.jsx';
 
 function AboutUs() {
+  const text =
+    'Join our team and change animals lives for the better! Together we can do more!';
   return (
     <div className={styles.wrapper}>
-      <div>
+      <div className={styles.section}>
         <div>
-          <h3>Who we are?</h3>
-          <p>
+          <h3 className={styles.title}>Who we are?</h3>
+          <p className={styles.text}>
             We are a community-driven platform designed to bridge the gap
             between animals looking for homes and the people who want to help
             them. Whether you are a pet lover searching for a new furry friend,
             a volunteer eager to make an impact, or a shelter seeking support,
             [Platform Name] is here to empower you.
           </p>
-          <p>
+          <p className={styles.text}>
             Our team is a passionate group of animal advocates who understand
             that collaboration is the key to addressing the challenges faced by
             homeless and abandoned animals. By bringing together individuals and
@@ -23,58 +26,59 @@ function AboutUs() {
           </p>
         </div>
 
-        <img src={temporaryImage} alt="#" />
+        <img className={styles.image} src={temporaryImage} alt="#" />
       </div>
-      <div>
-        <img src={temporaryImage} alt="#" />
+      <div className={styles.section}>
+        <img className={styles.image} src={temporaryImage} alt="#" />
         <div>
-          <h3>What We Do?</h3>
-          <ol>
-            <li>
-              Connect People and Pets: our platform makes it easy to browse
+          <h3 className={styles.title}>What We Do?</h3>
+          <div>
+            <p className={styles.text}>
+              1. Connect People and Pets: our platform makes it easy to browse
               profiles of animals in need of a home. With detailed information
               and photos, you can find the perfect match for your family.
-            </li>
-            <li>
-              Support Shelters and Rescues: we work closely with shelters and
+            </p>
+            <p className={styles.text}>
+              2. Support Shelters and Rescues: we work closely with shelters and
               rescue organizations to provide them with tools, visibility, and
               resources to increase their reach and effectiveness.
-            </li>
-            <li>
-              Empower Volunteers: volunteers play a crucial role in animal
+            </p>
+            <p className={styles.text}>
+              3. Empower Volunteers: volunteers play a crucial role in animal
               welfare. We help connect willing volunteers with shelters and
               rescue missions that need their help, whether it is fostering,
               fundraising, or providing hands-on care.
-            </li>
-            <li>
-              Raise Awareness: through our platform, we aim to educate and
+            </p>
+            <p className={styles.text}>
+              4. Raise Awareness: through our platform, we aim to educate and
               inspire people about the importance of adoption, responsible pet
               ownership, and the role each of us can play in making the world a
               better place for animals.
-            </li>
-          </ol>
+            </p>
+          </div>
         </div>
       </div>
-      <div>
+      <div className={styles.section}>
         <div>
-          <h3>Why We Do It?</h3>
-          <p>
+          <h3 className={styles.title}>Why We Do It?</h3>
+          <p className={styles.text}>
             Every year, millions of animals around the world end up in shelters
             or on the streets. Many of them face uncertain futures, but we
             believe that together, we can change that. By uniting efforts, we
             give these animals a second chance at life and a forever home where
             they can thrive.
           </p>
-          <h4>Join us</h4>
-          <p>
+          <h3 className={styles.title}>Join us</h3>
+          <p className={styles.text}>
             Whether you are looking to adopt, volunteer, or simply learn more
             about how you can help, [Platform Name] is your partner in making a
             difference. Together, we can create a world where every animal is
             loved, valued, and cared for.
           </p>
         </div>
-        <img src={temporaryImage} alt="#" />
+        <img className={styles.image} src={temporaryImage} alt="#" />
       </div>
+      <JoinUsSection text={text} />
     </div>
   );
 }
