a58cae8a164fa20474009b21b4ad1291cc9dde5b
KLEugene <kaeugene95@gmail.com>
2025-08-04 18:21:33 +0100

    translated join-us and subscription form

--- src/components/HomeComponents/JoinUsSection/JoinUsSection.jsx
+++ src/components/HomeComponents/JoinUsSection/JoinUsSection.jsx
@@ -1,22 +1,21 @@
 import { useNavigate } from 'react-router';
 import Button from '../../UI/Button.jsx';
+import { useTranslation } from 'react-i18next';
 import Section from '../../../layout/Section/Section.jsx';
 import styles from './JoinUsSection.module.css';
 
 function JoinUsSection() {
   const navigate = useNavigate();
+  const { t } = useTranslation();
   const handleJoinUs = () => {
     navigate('/sign-in');
   };
   return (
     <Section>
       <div className={styles.wrapper}>
-        <h2 className={styles.title}>
-          Join our team and change animals lives for the better! Together we can
-          do more!
-        </h2>
+        <h2 className={styles.title}>{t('home.join-us.description')}</h2>
         <Button className={styles.button} onClick={handleJoinUs}>
-          Join us
+          {t('home.join-us.button')}
         </Button>
       </div>
     </Section>
